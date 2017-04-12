32c5642fd8d0121f99764843a2f7551e66b9cd27
Xing Yi <xyplus@live.com>
2017-04-12 12:33:51 +0800

    Update

---
+++ ShowImg.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+output_path = "output\\"
+'''
+imgs = np.load(output_path+'images_0002_0025.npy')
+masks = np.load(output_path+'masks_0002_0025.npy')
+for i in range(len(imgs)):
+    print("image %d" % i)
+    fig, ax = plt.subplots(2, 2, figsize=[8, 8])
+    ax[0, 0].imshow(imgs[i], cmap='gray')
+    ax[0, 1].imshow(masks[i],cmap='gray')
+    ax[1,0].imshow(imgs[i]*masks[i],cmap='gray')
+    plt.show()
+'''
+
