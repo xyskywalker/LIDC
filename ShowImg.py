f2347ee3b1f40e38f2378d460da74d0fe64fcf0c
Xing Yi <xyplus@live.com>
2017-04-13 11:21:03 +0800

    Update

--- ShowImg.py
+++ ShowImg.py
@@ -2,7 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-output_path = "output\\"
+output_path = "traindata\\"
+imgfilename = output_path+'images_0025_0349.npy'
+v_centerfilename = imgfilename.replace("images", "v_center")
+img = np.load(imgfilename)
+v_center = np.load(v_centerfilename)
+plt.imshow(img)
+plt.plot(int(v_center[0]), int(v_center[1]), 'ro', color='red')
+plt.show()
+
+plt.imshow(img)
+plt.show()
 '''
 imgs = np.load(output_path+'images_0002_0025.npy')
 masks = np.load(output_path+'masks_0002_0025.npy')
